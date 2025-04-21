ec88afd8317126c56719a012f3be24ea382b836c
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-04-21 09:25:57 -0300

    initialize Spring Boot project with Maven wrapper and basic configuration for Emergy application

---
+++ src/main/java/br/com/example/emergia/EmergiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.emergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/emergia/EmergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.emergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

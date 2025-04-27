3f480ea163cd965c3987176b997aea6abfdc7b3b
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-04-27 08:57:15 -0300

    Add new data fetching functionality and update application properties for development

--- src/main/java/br/com/example/emergia/EmergiaApplication.java
+++ src/main/java/br/com/example/emergia/EmergiaApplication.java
@@ -1,29 +1,12 @@
 package br.com.example.emergia;
 
-import br.com.example.emergia.main.Main;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class EmergiaApplication implements CommandLineRunner {
-
-	private static ApplicationContext context;
+public class EmergiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmergiaApplication.class, args);
 	}
-
-	public static ApplicationContext getContext() {
-		return context;
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Main main = new Main();
-		main.main(args);
-	}
-
 }
---
+++ src/main/resources/static/JS/getDados.js
@@ -0,0 +1,10 @@
+const baseURL = 'http://localhost:8081/';
+
+export default function getDados(endpoint) {
+    return fetch(`${baseURL}${endpoint}`)
+        .then(responde => responde.json())
+        .catch(error => {
+            console.error('Erro ao buscar dados:', error);
+            throw error; // Re-throw the error for further handling if needed
+        })
+}

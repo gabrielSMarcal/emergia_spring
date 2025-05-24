81319ffc50aa80e11b8db7bd1d841150914d9079
PedroTCunha <teoficunha@gmail.com>
2025-05-24 17:16:53 -0300

    hotfix: fix the system that only allowed certain IPs

--- src/main/java/br/com/emergia/config/CorsConfig.java
+++ src/main/java/br/com/emergia/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5501")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
